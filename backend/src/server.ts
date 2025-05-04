9c521bbf1770c6cae887584dfe00369e521f6394
Almina Silnovic <alminasilnovic10@gmail.com>
2025-05-04 17:09:47 +0200

    feat(database): configure database connection with pool.ts, add environment variables, and install required packages

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,30 @@
-import express, {Request, Response} from 'express';
-import cors from 'cors';
+import express from 'express';
+import pool from './database/pool';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
 const port = 3000;
+const databaseUrl = process.env.DATABASE_URL;
 
-app.use(cors());
+app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+if (!databaseUrl) {
+    console.error('Database URL is missing from environment variables.');
+    process.exit(1);
+}
+
+app.get('/api/users', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM "users"');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Database query failed');
+    }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
